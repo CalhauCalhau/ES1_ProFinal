9daaea73be93cd4921e0b379b9d0c63f0dfcf7e0
miguelryzen <miguel.als98@gmail.com>
2019-06-26 16:03:00 +0100

    ES1_ProFinal_v1

--- src/Utilizador.java
+++ src/Utilizador.java
@@ -4,18 +4,18 @@ public class Utilizador
     String estado;
     TipoUtilizador tipoutilizador;
 
-    public Utilizador(String nome, String estado) {
-        this.nome = nome;
-        this.estado = estado;
-    }
-
     public String getNome() {
         return nome;
     }
 
-
     public String getEstado() {
         return estado;
+    }
+
+    public Utilizador(String nome, String estado, TipoUtilizador tipoutilizador) {
+        this.nome = nome;
+        this.estado = estado;
+        this.tipoutilizador = tipoutilizador;
     }
 
     public TipoUtilizador getTipoUtilizador() {
